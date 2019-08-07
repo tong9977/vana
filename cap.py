4036e526ad5fd0d655eec2dac28512849fe8bffe
Racha Monyanon <tong9977@gmail.com>
2019-08-07 17:17:46 +0700

    createBackgroundSubtractorMOG2
    createBackgroundSubtractorMOG2

--- cap.py
+++ cap.py
@@ -1,6 +1,32 @@
 import PyCapture2
 import cv2
 import numpy as np
+import time
+import shutil
+
+def bb_intersection_over_union(boxA, boxB):
+	# determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+ 
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+ 
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+ 
+	# return the intersection over union value
+	return iou
+
 
 def print_build_info():
     lib_ver = PyCapture2.getLibraryVersion()
@@ -28,6 +54,21 @@ def enable_embedded_timestamp(cam, enable_timestamp):
         else:
             print('\nTimeStamp is disabled.\n')
 
+
+def grab(cam):
+
+    cam.startCapture()
+    image = cam.retrieveBuffer()
+    cam.stopCapture()
+    timestamp = image.getTimeStamp()
+
+
+    image.save('tmp.bmp'.encode('utf-8'),PyCapture2.IMAGE_FILE_FORMAT.BMP)
+    img = cv2.imread('tmp.bmp',0)
+  
+    return img,timestamp,image
+
+
 def grab_images(cam, num_images_to_grab):
     prev_ts = None
     for i in range(num_images_to_grab):
@@ -43,9 +84,6 @@ def grab_images(cam, num_images_to_grab):
             print('Timestamp [ %d %d ] - %d' % (ts.cycleSeconds, ts.cycleCount, diff))
         prev_ts = ts
 
-    image.save('tmp.bmp'.encode('utf-8'),PyCapture2.IMAGE_FILE_FORMAT.BMP)
-    data = cv2.imread('tmp.bmp',0)
-    cv2.imshow('img',data)
 
     #newimg = image.convert(PyCapture2.PIXEL_FORMAT.BGR)
     #print('Saving the last image to fc2TestImage.png')
@@ -73,20 +111,45 @@ print_camera_info(c)
 
 # Enable camera embedded timestamp
 enable_embedded_timestamp(c, True)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 print('Starting image capture...')
-c.startCapture()
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
 while 1:
-    grab_images(c, 1)
+    #grab_images(c, 1)
+    img,ts,orgImage = grab(c)
+
+    
+    fgmask = fgbg.apply(img)
+    count = cv2.countNonZero(fgmask)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img,'Timestamp [ {:04d} {:04d} {}]'.format(ts.cycleSeconds, ts.cycleCount,count),(20,20), font, .5,(255,255,255),1,cv2.LINE_AA)
+    cv2.imshow('innertube',img)
+    #cv2.imshow('fgmask',fgmask)
+    
+    if count>=80000 :
+        filename ='{:04d}-{:04d}.png '.format(ts.cycleSeconds, ts.cycleCount)
+    
+        newimg = orgImage.convert(PyCapture2.PIXEL_FORMAT.BGR)
+        print('Saving {}'.format(filename))
+        newimg.save('tmp.png'.encode('utf-8'), PyCapture2.IMAGE_FILE_FORMAT.PNG)
+        newPath = shutil.copy('tmp.png', './test/{}'.format(filename))
+
+    
+    print(count)
+
+    #cv2.imwrite('{:04d}-{:04d}.png '.format(ts.cycleSeconds, ts.cycleCount),fgmask)
+    #time.sleep(1)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
     
-c.stopCapture()
 
 # Disable camera embedded timestamp
-enable_embedded_timestamp(c, False)
+enable_embedded_timestamp(c, True)
 c.disconnect()
 
 #input('Done! Press Enter to exit...\n')
