92d065d482ea5930b5ebdd60cb42fef26bebee16
Rachakrit Monyanon <tong9977@gmail.com>
2019-09-03 22:23:37 +0700

    rfid

---
+++ station/rfid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import http.client
+import json
+import time
+
+while True:
+    #rfid = input("My RFID: ") 
+    rfid = "Test5"
+    print(rfid)
+    connection = http.client.HTTPConnection("192.168.1.102:3030")
+    #connection = http.client.HTTPConnection("192.168.111.19:3030")
+    headers = {'Content-type':'application/json'}
+    scandata = {'TagNo':rfid,'StationId':1}
+    json_scandata = json.dumps(scandata)
+    connection.request("POST", "/scandata",json_scandata,headers)
+    response = connection.getresponse()
+    print(response.read().decode())
+    time.sleep(5)
+    #rfid = input("My RFID: ") 
+
+connection.close()
